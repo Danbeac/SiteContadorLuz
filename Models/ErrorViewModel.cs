395c54f732dd15eed1793933d6edea1099f28988
Andres Bernal <danbeac@hotmail.com>
2020-08-07 20:59:56 -0500

    Primer commit se suben archivo MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Contador_Luz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
